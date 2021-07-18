8391268157f02270ffa5703f7f2ea411253f1fbb
Igor Overchuk <igor.t.over@gmail.com>
2021-07-17 21:12:26 -0600

    react frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className = 'container'>
+      <Header title = 'hello' value = {2} />
+    </div>
+  );
+}
+
+export default App;
